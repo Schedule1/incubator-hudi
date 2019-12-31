a51c5fadecb2345881e99895ed6659f6648a67d1
lamber-ken <lamberken@163.com>
2019-12-30 21:44:56 -0600

    [HUDI-482] Fix missing @Override annotation on methods (#1156)
    * [HUDI-482] Fix missing @Override annotation on methods

--- hudi-common/src/main/java/org/apache/hudi/common/util/ObjectSizeCalculator.java
+++ hudi-common/src/main/java/org/apache/hudi/common/util/ObjectSizeCalculator.java
@@ -136,6 +136,7 @@ public class ObjectSizeCalculator {
 
   private final LoadingCache<Class<?>, ClassSizeInfo> classSizeInfos =
       CacheBuilder.newBuilder().build(new CacheLoader<Class<?>, ClassSizeInfo>() {
+        @Override
         public ClassSizeInfo load(Class<?> clazz) {
           return new ClassSizeInfo(clazz);
         }
